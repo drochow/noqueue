ca3df51fd0b829373378aaf992b3bc92e2f839c5
Sean-Percy Harnack <s58562@beuth-hochschule.de>
2016-11-07 10:22:13 +0100

    Update Anbieter.scala
    changed package name from "models2" to models

--- backend/app/models/Anbieter.scala
+++ backend/app/models/Anbieter.scala
@@ -7,20 +7,19 @@ import slick.lifted.{ProvenShape, ForeignKeyQuery}
 
 import scala.concurrent.Future
 
-  case class Anbieter(id: Option[Long], anwenderID: Long, anbieterID: Long)
+case class Anbieter(id: Option[Long], anwenderID: Long, anbieterID: Long)
 
-  class Anbieters(tag: Tag) extends Table[Anbieter](tag, "ANBIETER") {
-    def id = column[Long]("LEI_ID", O.PrimaryKey, O.AutoInc)
-    def adresseID = column[Long]("ADR_ID")
-    def tel = column[String]("TEL")
-    def oeffnungszeiten = column[String]("OEFFNUNGSZEITEN")
-    def kontaktEmail = column[String]("KONTAKTEMAIL")
-    def wsOffen = column[Boolean]("WSOFFEN")
-    def bewertung = column[Int]("BEWERTUNG")
+class Anbieters(tag: Tag) extends Table[Anbieter](tag, "ANBIETER") {
+  def id = column[Long]("LEI_ID", O.PrimaryKey, O.AutoInc)
+  def adresseID = column[Long]("ADR_ID")
+  def tel = column[String]("TEL")
+  def oeffnungszeiten = column[String]("OEFFNUNGSZEITEN")
+  def kontaktEmail = column[String]("KONTAKTEMAIL")
+  def wsOffen = column[Boolean]("WSOFFEN")
+  def bewertung = column[Int]("BEWERTUNG")
 
-    def * = (id.?, adresseID, tel, oeffnungszeiten, kontaktEmail, wsOffen, bewertung) <> (Anbieter.tupled, Anbieter.unapply)
+  def * = (id.?, adresseID, tel, oeffnungszeiten, kontaktEmail, wsOffen, bewertung) <> (Anbieter.tupled, Anbieter.unapply)
 
-    def adresse: ForeignKeyQuery[Adresses, Adresse] =
-      foreignKey("ADR_FK", adresseID, TableQuery[Adresses])(_.id)
-  }
+  def adresse: ForeignKeyQuery[Adresses, Adresse] = foreignKey("ADR_FK", adresseID, TableQuery[Adresses])(_.id)
+}
 
