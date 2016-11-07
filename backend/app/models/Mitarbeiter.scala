aab54e5ca497d6ca83ab45b8f074170b0f6b369c
Sean-Percy Harnack <s58562@beuth-hochschule.de>
2016-11-07 10:23:30 +0100

    Update Mitarbeiter.scala

--- backend/app/models/Mitarbeiter.scala
+++ backend/app/models/Mitarbeiter.scala
@@ -7,18 +7,18 @@ import slick.lifted.{ProvenShape, ForeignKeyQuery}
 
 import scala.concurrent.Future
 
-  case class Mitarbeiter(id: Option[Long], anwenderID: Long, anbieterID: Long, anwesend: Boolean)
+case class Mitarbeiter(id: Option[Long], anwenderID: Long, anbieterID: Long, anwesend: Boolean)
 
-  class Mitarbeiters(tag: Tag) extends Table[Mitarbeiter](tag, "MITARBEITER") {
-    def id = column[Long]("MIT_ID", O.PrimaryKey, O.AutoInc)
-    def anbieterID = column[Long]("ANB_ID")
-    def anwenderID = column[Long]("ANW_ID")
-    def anwesend = column[Boolean]("ANWESEND")
+class Mitarbeiters(tag: Tag) extends Table[Mitarbeiter](tag, "MITARBEITER") {
+  def id = column[Long]("MIT_ID", O.PrimaryKey, O.AutoInc)
+  def anbieterID = column[Long]("ANB_ID")
+  def anwenderID = column[Long]("ANW_ID")
+  def anwesend = column[Boolean]("ANWESEND")
 
-    def * = (id.?, anbieterID, anwenderID, anwesend) <> (Mitarbeiter.tupled, Mitarbeiter.unapply)
+  def * = (id.?, anbieterID, anwenderID, anwesend) <> (Mitarbeiter.tupled, Mitarbeiter.unapply)
 
-    def anbieter: ForeignKeyQuery[Anbieters, Anbieter] =
-      foreignKey("ANB_FK", anbieterID, TableQuery[Anbieters])(_.id)
-    def anwender: ForeignKeyQuery[Anwenders, Anwender] =
-      foreignKey("ANW_FK", anwenderID, TableQuery[Anwenders])(_.id)
-  }
+  def anbieter: ForeignKeyQuery[Anbieters, Anbieter] =
+    foreignKey("ANB_FK", anbieterID, TableQuery[Anbieters])(_.id)
+  def anwender: ForeignKeyQuery[Anwenders, Anwender] =
+    foreignKey("ANW_FK", anwenderID, TableQuery[Anwenders])(_.id)
+}
