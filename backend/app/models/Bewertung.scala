9eb1bf88517fa7f05974fbb812f7a7aadab5fb60
Sean-Percy Harnack <s58562@beuth-hochschule.de>
2016-11-07 10:20:30 +0100

    Update Bewertung.scala
    changed package name from "models2" to models

--- backend/app/models/Bewertung.scala
+++ backend/app/models/Bewertung.scala
@@ -1,4 +1,4 @@
-package models2
+package models
 
 import slick.driver.H2Driver.api._
 import slick.lifted.TableQuery
@@ -7,19 +7,19 @@ import slick.lifted.{ProvenShape, ForeignKeyQuery}
 
 import scala.concurrent.Future
 
-  case class Bewertung(id: Option[Long], anbieterID: Long, anwenderID: Long, wert: Int)
+case class Bewertung(id: Option[Long], anbieterID: Long, anwenderID: Long, wert: Int)
 
-  class Bewertungs(tag: Tag) extends Table[Bewertung](tag, "BEWERTUNG") {
-    def id = column[Long]("BEW_ID", O.PrimaryKey, O.AutoInc)
-    def anbieterID = column[Long]("ANB_ID")
-    def anwenderID = column[Long]("ANW_ID")
-    def wert = column[Int]("WERT")
+class Bewertungs(tag: Tag) extends Table[Bewertung](tag, "BEWERTUNG") {
+  def id = column[Long]("BEW_ID", O.PrimaryKey, O.AutoInc)
+  def anbieterID = column[Long]("ANB_ID")
+  def anwenderID = column[Long]("ANW_ID")
+  def wert = column[Int]("WERT")
 
-    def * = (id.?, anbieterID, anwenderID, wert) <> (Bewertung.tupled, Bewertung.unapply)
+  def * = (id.?, anbieterID, anwenderID, wert) <> (Bewertung.tupled, Bewertung.unapply)
 
-    def anbieter: ForeignKeyQuery[Anbieters, Anbieter] =
-      foreignKey("ANB_FK", anbieterID, TableQuery[Anbieters])(_.id)
+  def anbieter: ForeignKeyQuery[Anbieters, Anbieter] =
+    foreignKey("ANB_FK", anbieterID, TableQuery[Anbieters])(_.id)
 
-    def anwender: ForeignKeyQuery[Anwenders, Anwender] =
-      foreignKey("ANW_FK", anwenderID, TableQuery[Anwenders])(_.id)
-  }
+  def anwender: ForeignKeyQuery[Anwenders, Anwender] =
+    foreignKey("ANW_FK", anwenderID, TableQuery[Anwenders])(_.id)
+}
