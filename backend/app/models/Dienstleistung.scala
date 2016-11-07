33db2f1e65047faf1e0320f30a3be4b0df5231b7
Sean-Percy Harnack <s58562@beuth-hochschule.de>
2016-11-07 10:20:04 +0100

    Update Dienstleistung.scala
    changed package name from "models2" to models

--- backend/app/models/Dienstleistung.scala
+++ backend/app/models/Dienstleistung.scala
@@ -1,4 +1,4 @@
-package models2
+package models
 
 import slick.driver.H2Driver.api._
 import slick.lifted.TableQuery
@@ -7,19 +7,19 @@ import slick.lifted.{ProvenShape, ForeignKeyQuery}
 
 import scala.concurrent.Future
 
-  case class Dienstleistung(id: Option[Long], dlTypID: Long, anbieterID: Long, kommentar: String, aktion: String)
+case class Dienstleistung(id: Option[Long], dlTypID: Long, anbieterID: Long, kommentar: String, aktion: String)
 
-  class Dienstleistungs(tag: Tag) extends Table[Dienstleistung](tag, "DIENSTLEISTUNG") {
-    def id = column[Long]("DL_ID", O.PrimaryKey, O.AutoInc)
-    def dlTypID = column[Long]("DLT_ID")
-    def anbieterID = column[Long]("ANB_ID")
-    def kommentar = column[String]("KOMMENTAR")
-    def aktion = column[String]("AKTION")
+class Dienstleistungs(tag: Tag) extends Table[Dienstleistung](tag, "DIENSTLEISTUNG") {
+  def id = column[Long]("DL_ID", O.PrimaryKey, O.AutoInc)
+  def dlTypID = column[Long]("DLT_ID")
+  def anbieterID = column[Long]("ANB_ID")
+  def kommentar = column[String]("KOMMENTAR")
+  def aktion = column[String]("AKTION")
 
-    def * = (id.?, dlTypID, anbieterID, kommentar, aktion) <> (Dienstleistung.tupled, Dienstleistung.unapply)
+  def * = (id.?, dlTypID, anbieterID, kommentar, aktion) <> (Dienstleistung.tupled, Dienstleistung.unapply)
 
-    def dienstleistungsTyp: ForeignKeyQuery[DienstleistungsTyps, DienstleistungsTyp] =
-      foreignKey("DLT_FK", dlTypID, TableQuery[DienstleistungsTyps])(_.id)
-    def anbieter: ForeignKeyQuery[Anbieters, Anbieter] =
-      foreignKey("ANB_FK", anbieterID, TableQuery[Anbieters])(_.id)
-  }
+  def dienstleistungsTyp: ForeignKeyQuery[DienstleistungsTyps, DienstleistungsTyp] =
+    foreignKey("DLT_FK", dlTypID, TableQuery[DienstleistungsTyps])(_.id)
+  def anbieter: ForeignKeyQuery[Anbieters, Anbieter] =
+    foreignKey("ANB_FK", anbieterID, TableQuery[Anbieters])(_.id)
+}
