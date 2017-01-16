66cfdd9b9bcbf99239f547b1eb82518ab7fcd887
David Kaatz <david.kaatz@rocket-internet.de>
2017-01-16 18:55:12 +0100

    [Backend] Fixed type issue

--- backend/app/models/db/WarteschlangenPlatzComponent.scala
+++ backend/app/models/db/WarteschlangenPlatzComponent.scala
@@ -103,7 +103,7 @@ trait WarteschlangenPlatzComponent {
   def delete(wspId: PK[WarteschlangenPlatzEntity], anwID: PK[AnwenderEntity]) =
     for {
       wsp <- warteschlangenplaetze.filter(_.id === wspId).result.head
-      prevWsp <- warteschlangenplaetze.filter(_.folgePlatzId === wsp.id).map(_.folgePlatzId).update(wsp.folgeNummer.get)
+      prevWsp <- warteschlangenplaetze.filter(_.folgePlatzId === wsp.id).map(_.folgePlatzId).update(wsp.folgeNummer)
       del <- warteschlangenplaetze.filter(_.id === wspId).filter(_.anwenderId === anwID).delete
     } yield del
 
