6d7f49d4958697d818e2b85992a380f70db97826
David Kaatz <david.kaatz@rocket-internet.de>
2017-01-25 15:03:21 +0100

    [Backend] changed Guice Module Loader to use mode to distungish between test and dev/prod. environment

--- backend/app/modules/NoQueueModule.scala
+++ backend/app/modules/NoQueueModule.scala
@@ -4,21 +4,19 @@ import com.google.inject.AbstractModule
 import models.{ DB, H2DB, PostgresDB }
 import osm.{ AdressService, GoolgeAdressService, OSMAdressService }
 import play.api.inject.ApplicationLifecycle
-import play.api.{ Configuration, Environment }
+import play.api.{ Configuration, Environment, Mode }
 
 /**
  * Created by David on 09.01.17.
  */
 class NoQueueModule(environment: Environment, configuration: Configuration) extends AbstractModule {
   override def configure() = {
-    bind(classOf[AdressService]).to(classOf[OSMAdressService])
-    bind(classOf[DB]).to(classOf[PostgresDB])
-  }
-}
-
-class NoQueueTestModule(environment: Environment, configuration: Configuration) extends AbstractModule {
-  override def configure() = {
-    bind(classOf[AdressService]).to(classOf[GoolgeAdressService])
-    bind(classOf[DB]).to(classOf[H2DB])
+    if (environment.mode.equals(Mode.Test)) {
+      bind(classOf[AdressService]).to(classOf[GoolgeAdressService])
+      bind(classOf[DB]).to(classOf[H2DB])
+    } else {
+      bind(classOf[AdressService]).to(classOf[OSMAdressService])
+      bind(classOf[DB]).to(classOf[PostgresDB])
+    }
   }
 }
