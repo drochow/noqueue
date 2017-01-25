ab552462100c4fd67c800e244ae78df5599154de
Benj_S <bschurian4@gmail.com>
2017-01-25 17:02:03 +0100

    AnwTest

---
+++ backend/test/AnwenderSpec.scala
@@ -0,0 +1,9 @@
+import org.scalatest.{ Assertion, AsyncFlatSpec, AsyncWordSpec, FutureOutcome }
+/**
+  * Created by anwender on 25.01.2017.
+  */
+class AnwenderSpec extends AsyncFlatSpec{
+  override def withFixture(test: NoArgAsyncTest): FutureOutcome = super.withFixture(test)
+
+
+}
