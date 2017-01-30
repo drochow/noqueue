42f03320e65920f5e7b2aa19abc4647e9a3c6070
Benj_S <bschurian4@gmail.com>
2017-01-30 09:46:06 +0100

    tests

--- backend/test/AnwenderSpec.scala
+++ backend/test/AnwenderSpec.scala
@@ -1,6 +1,6 @@
 import models.db.{ AdresseEntity, AnwenderEntity }
 import models.{ Anwender, DB, UnregistrierterAnwender }
-import org.scalatest.{ Assertion, AsyncFlatSpec, AsyncWordSpec, FutureOutcome }
+import org.scalatest._
 import org.scalatest.Matchers._
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import play.api.Mode
@@ -11,23 +11,67 @@ import scala.concurrent.Future
 /**
  * Created by anwender on 25.01.2017.
  */
-class AnwenderSpec extends AsyncFlatSpec {
+class AnwenderSpec extends AsyncFlatSpec with ParallelTestExecution {
   override def withFixture(test: NoArgAsyncTest): FutureOutcome = super.withFixture(test)
-
 
   val application = new GuiceApplicationBuilder()
     .in(Mode.Test)
     .build
   val db: DB = application.injector.instanceOf[DB]
-  val al: ApplicationLifecycle = application.injector.instanceOf[ApplicationLifecycle]
-  val uA = new UnregistrierterAnwender(al, db);
-  val anwE: AnwenderEntity = AnwenderEntity("bang@example.com", "password1234", "bangNutzer")
-  val addrE: AdresseEntity = AdresseEntity("", "", "", "", None, None)
-  //val anw: Future[Anwender] = uA.registrieren(anwE)
+  //db-setup
+  var test = "before creation of the DAL"
+  //initilisation of our dal, if we're using a h2-DB in Memory we don't need to set that up
+  db.db.run(db.dal.create) map {
+    _ => { test = "after successful creation of the DAL" }
+  } recover {
+    case err: Throwable => test = "after failed creation of the DAL"
+  }
+  //val al: ApplicationLifecycle = application.injector.instanceOf[ApplicationLifecycle]
+  //val addrE: AdresseEntity = AdresseEntity("", "", "", "", None, None)
+
+  def anwenderize(x: Any) = {
+    AnwenderEntity(x + "@example.com", "password" + x, "User" + x)
+  }
+  val anwenderEs = (1 to 10).toList.map(anwenderize)
+
+  val uA = new UnregistrierterAnwender(db);
+  def persist(anwenderEntity: AnwenderEntity) = {
+    uA.registrieren(anwenderEntity)
+  }
+  val persAnwenders = anwenderEs.map(persist)
+
+  def modelFromPersistedAnw(pAnw: Future[AnwenderEntity]) = {
+    pAnw map {
+      case anwE => new Anwender(db.dal.getAnwenderWithAdress(anwE.id.get), db)
+    }
+  }
+  val anwenderModels = persAnwenders.map(modelFromPersistedAnw)
 
   "An Anwender" should "return his profile" in {
-    //anw.map(_.profilAnzeigen() should ===(anwE, None))
+    /*for {
+      profil <- anwenderModels(0).map(_.profilAnzeigen())
+      persistedAnwender <- persAnwenders(0)
+    } yield (profil should ===(persistedAnwender, None))*/
+    anwenderModels map (_ map (_.profilAnzeigen().map(println(_))))
+    //persAnwenders map (_ map println(_))
+    succeed
   }
-  an[NoSuchElementException] should be thrownBy {
+  it should "permit full-on-changing but still respect uniqueness" in {
+    val s = "Update010"
+    val anwenderEntity = anwenderize(s)
+    for {
+      anwender <- anwenderModels(1)
+      updated <- anwender.anwenderInformationenAustauschen(anwenderEntity, None)
+      test <- Future.successful(if (!updated) Failed)
+      profil <- anwender.profilAnzeigen()
+      updatedAnw <- Future.successful {
+        profil match {
+          case (anw: AnwenderEntity, _) => anw
+        }
+      }
+    } yield (updatedAnw.nutzerName should ===(anwenderEntity.nutzerName))
+    succeed
   }
+  /*an [NoSuchElementException] should be thrownBy {
+  }*/
 }
